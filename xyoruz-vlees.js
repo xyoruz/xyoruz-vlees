c25a241549b02f6370e2c1e48fe3a01e173e9f87
XYR <169094507+xyoruz@users.noreply.github.com>
2024-12-31 19:27:20 +0700

    Update xyoruz-vlees.js

--- xyoruz-vlees.js
+++ xyoruz-vlees.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '77284226-4b80-4a9d-93ee-625dc0c54a4f';
 
-let proxyIP = ['proxyip'];
+let proxyIP = ['146.56.162.25'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
